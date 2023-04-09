87bbce972026325372b525df58fb3b8e573c6bee
l3andr3s <128740599+l3andr3s@users.noreply.github.com>
2023-04-08 22:19:51 -0400

    Add files via upload
    arreglo de clase cliente

---
+++ ProyectF/src/logico/Cliente.java
@@ -0,0 +1,51 @@
+package logico;
+
+public class Cliente {
+
+	private String cedula;
+	private String nombre;
+	private String telefono;
+	private String direccion;
+	
+	public Cliente(String cedula, String nombre, String telefono, String direccion) {
+		super();
+		this.cedula = cedula;
+		this.nombre = nombre;
+		this.telefono = telefono;
+		this.direccion = direccion;
+	}
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	
+
+}
