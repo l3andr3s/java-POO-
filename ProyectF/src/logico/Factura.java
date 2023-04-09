3aa104d0421aaf545cc83aa49896059262da83b9
Lenovo <Lenovo@192.168.0.103>
2023-04-08 22:44:31 -0400

    Creando clase memoriaram

--- ProyectF/src/logico/Factura.java
+++ ProyectF/src/logico/Factura.java
@@ -2,5 +2,10 @@ package logico;
 
 public class Factura {
 	private Cliente cliente;
-
+	public Factura(Cliente cliente, Combo combo, ArrayList<Componente> componentes) {
+		super();
+		
+	}		
 }
+	
+
---
+++ ProyectF/src/logico/MemoriaRam.java
@@ -0,0 +1,6 @@
+package logico;
+
+public class MemoriaRam extends Componente {
+	private float memoria;
+	private String tipoMemoria;
+}
